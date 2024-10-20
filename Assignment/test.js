ba2b0ada5d15cddf9d2a369e74aa6527fe5b3ee0
Ankhbayar <zanhaa56@gmail.com>
2024-10-20 16:11:15 +0800

    assignment first

--- Assignment/test.js
+++ Assignment/test.js
@@ -1,3 +1,12 @@
-console.log("hey nigga")
-let test = document.getElementById("test")
-test.style.backgroundColor = "black"
+const nav = document.querySelector('#header_container');
+const footer = document.querySelector('#footer_container');
+fetch('/Header.html')
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data;
+  });
+fetch('/Footer.html')
+  .then(res => res.text())
+  .then(data =>{
+    footer.innerHTML = data;
+  })
